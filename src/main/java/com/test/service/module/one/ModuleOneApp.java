d60681bc2af28c751fbd37f0feaa4f2274e23f3c
jorge-dutton <jorge.dutton@gmail.com>
2021-07-02 09:07:33 +0200

    some changes

--- src/main/java/com/test/service/module/one/ModuleOneApp.java
+++ src/main/java/com/test/service/module/one/ModuleOneApp.java
@@ -4,6 +4,8 @@ public class ModuleOneApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//JAdESSignatureParameters parameters = new JAdESSignatureParameters();
+		//PAdESSignatureParameters parameters = new PAdESSignatureParameters();
 
 	}
 
