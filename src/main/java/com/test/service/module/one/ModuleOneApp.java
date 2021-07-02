ff2afa7d6af3a27f182a23b0a5ac8612dfb7c313
jorge-dutton <jorge.dutton@gmail.com>
2021-07-02 12:39:17 +0200

    test changes

--- src/main/java/com/test/service/module/one/ModuleOneApp.java
+++ src/main/java/com/test/service/module/one/ModuleOneApp.java
@@ -1,11 +1,13 @@
 package com.test.service.module.one;
 
+import eu.europa.esig.dss.pades.PAdESSignatureParameters;
+
 public class ModuleOneApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//JAdESSignatureParameters parameters = new JAdESSignatureParameters();
-		//PAdESSignatureParameters parameters = new PAdESSignatureParameters();
+		PAdESSignatureParameters parameters = new PAdESSignatureParameters();
 
 	}
 
